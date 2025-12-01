18950eec8c8be654a0c0cb1523d0cfa541a96d01
unknown <ms25aft@herts.ac.uk>
2025-12-02 00:31:56 +0530

    Data Preparation

--- data_preparation.R
+++ data_preparation.R
@@ -5,3 +5,4 @@ df$week_number <- as.numeric(gsub("w", "", df$week))
 df$period <- ifelse(df$week_number >= 1 & df$week_number <= 12, "Early (1-12)",
 ifelse(df$week_number >= 13 & df$week_number <= 24, "Later (13-24)", "Other"))
 df2 <- subset(df, state == "Malaysia" & period != "Other")
+
