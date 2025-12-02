d8973abafbb483216e22d0073fc90fde13819121
haseebherts <ah25aba@herts.ac.uk>
2025-12-02 22:27:11 +0000

    Add files via upload

--- data_preparation_updated_steps.R
+++ data_preparation_updated_steps.R
@@ -24,3 +24,13 @@ wilcox_result <- wilcox.test(df2$perc_18_29 ~ df2$period)
 print(wilcox_result)
 median_values <- aggregate(perc_18_29 ~ period, df2, median)
 print(median_values)
+
+
+# Part 5: Visualization and Final Interpretation
+boxplot(df2$perc_18_29 ~ df2$period,
+        xlab = "Pandemic Period", 
+        ylab = "Proportion of Cases (%)",
+        main = "Comparison of COVID-19 Case Proportion (Age 18-29)",
+        col = c("lightblue", "lightgreen"))
+
+
