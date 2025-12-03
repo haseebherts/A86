896bd3ef3bc99aa7ac18945f90b525966b6de0d7
unknown <ms25aft@herts.ac.uk>
2025-12-03 21:40:30 +0530

    t-test and env

--- t_test.R
+++ t_test.R
@@ -3,7 +3,7 @@ covid_data <- read_csv("cases_age.csv")
 df <- covid_data
 df$week_number <- as.numeric(gsub("w", "", df$week))
 df$period <- ifelse(df$week_number >= 1 & df$week_number <= 12, "Early (1-12)",
-ifelse(df$week_number >= 13 & df$week_number <= 24, "Later (13-24)", "Other"))
+                    ifelse(df$week_number >= 13 & df$week_number <= 24, "Later (13-24)", "Other"))
 df2 <- subset(df, state == "Malaysia" & period != "Other")
 save.image("D:/Shaalan/A86_new/env.RData")
 t_test_result <- t.test(df2$perc_18_29 ~ df2$period)
