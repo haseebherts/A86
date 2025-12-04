373db8553bd40bea79ac6c3a35bf70e81acf6c99
muhammadumarmehboob <mm25aif@herts.ac.uk>
2025-12-04 14:27:19 +0000

    data-preparation Johor

--- data_preparation.R
+++ data_preparation.R
@@ -4,4 +4,4 @@ df <- covid_data
 df$week_number <- as.numeric(gsub("w", "", df$week))
 df$period <- ifelse(df$week_number >= 1 & df$week_number <= 12, "Early (1-12)",
 ifelse(df$week_number >= 13 & df$week_number <= 24, "Later (13-24)", "Other"))
-df2 <- subset(df, state == "Malaysia" & period != "Other")
+df2 <- subset(df, state == "Johor" & period != "Other")
