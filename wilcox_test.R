c2a411b2538671f32463f5a0430343afd190ac20
STORMNIGHT\Abdul Haseeb <haseeb@sudo.ae>
2025-12-04 14:56:20 +0000

    wilcox_test_update

---
+++ wilcox_test.R
@@ -0,0 +1,5 @@
+# Part 4: Non-parametric Testing
+wilcox_result <- wilcox.test(df2$perc_18_29 ~ df2$period)
+print(wilcox_result)
+median_values <- aggregate(perc_18_29 ~ period, df2, median)
+print(median_values)
